f325222c97de905c3d3427045e62c264d3c09594
Scott Laidlaw <scottlaidlaw@protonmail.com>
2022-02-28 14:34:13 -0800

    api seems like a better bet, i think i may scrap selenium. people keep telling me about praw, but i think ill learn more doing it with requests.

--- rst(reddit-api).py
+++ rst(reddit-api).py
@@ -1,41 +1,53 @@
-import pwinput
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import requests
+import json
+import os
 
-driver = webdriver.Firefox()
-def logonReddit(driver):
-    userLogon = 0
-    while userLogon != 1:
-        driver.get("https://old.reddit.com")
-        elemInput = driver.find_element_by_name("user")
-        userName = input('Reddit Username:')
-        elemInput.send_keys(userName)
-        elemInput = driver.find_element_by_name("passwd")
-        elemInput.clear()
-        userPass = pwinput.pwinput()
-        elemInput.send_keys(userPass)
-        elemWrong = driver.find_element_by_class_name("submit").click
-        driver.find_element_by_link_text("https://old.reddit.com/user/Someonespants/")
-        if logonCheck == userName:
-            print("gay boobs")
-        else:
-            userLogon = 1
-            elemInput.clear()
-            print ("stray boobs")
-def gatherSubList(driver):
-        subList = driver.find_elements_by_id("srList")
-        links = subList.find_elements_by_tag_name("a")
-        for link in links:
-            if links[0:3] == "/r/":
-                out.append(link.get_attribute(href) + "/" )
-                print(out)
-            else:
-                next (link)
 
-def rejoinsubReddits(driver):
-    driver.find_element_by_class_name("option remove login-required active")
-    driver.find_element_by_class_name("option add active")
-logonReddit(driver)
-gatherSubList
+def getLogins():
+    
+    #uncomment if you would like to enter your api keys in the python terminal
+    
+    #import pwinput
+    #oldClient = pwinput.pwinput(prompt = 'client id for old account: ')
+    #oldSecret = pwinput.pwinput(prompt = 'client secret for old account: ')
+    #oldUsername = input('username for old account: ')
+    #oldPassword = pwinput.pwinput(prompt = 'password for old account: ')
+    #newClient = pwinput.pwinput(prompt = 'client id for new account: ')
+    #newSecret = pwinput.pwinput(prompt = 'client secret for new account: ')
+    #newUsername = input('username for new account: ')
+    #newPassword = pwinput.pwinput(prompt = 'password for new account: ')
+    #api_list = [oldClient, oldSecret, oldUsername, oldPassword, newClient, newSecret, newUsername, newPassword]
 
+    #if you would like to use an env variable in the format of "oldclient/oldsecret/oldusername/oldpassword/newclient/newsecret/newusername/newpassword" (w/o quotes)
+    
+    api_String = os.environ["api_login"]
+    api_list = api_String.split("/", -1)
+    
+    return(api_list)
+
+def getSubs(api_list):
+
+    auth = requests.auth.HTTPBasicAuth(api_list[0], api_list[1])
+
+    data = {
+        'grant_type' : 'password',
+        'username' : api_list[2],
+        'password' : api_list[3]
+        }
+    headers = {'User-Agent' : 'FluentKopimist_Sub-Transfer_API/0.0.1}'}
+
+    res = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
+
+    TOKEN = res.json()['access_token']
+
+    headers = {**headers, **{'Authorization':f'bearer {TOKEN}'}}
+
+    res = requests.get('https://oauth.reddit.com/subreddits/mine/subscriber', headers=headers)
+    
+    for post in res.json()['data']['children']:
+        
+
+    
+
+api_list = getLogins()
+getSubs(api_list)
